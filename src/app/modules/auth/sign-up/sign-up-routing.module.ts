849cd12a4667955596a019d32368cb1057daab16
Experts Web 2 <experts.web2@gmail.com>
2023-07-19 20:40:52 +0500

    Lab_Order_Completed

--- src/app/modules/auth/sign-up/sign-up-routing.module.ts
+++ src/app/modules/auth/sign-up/sign-up-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up.component';
 
-const routes: Routes = [{ path: '', component: SignUpComponent }];
+const routes: Routes = [
+  { path: '', component: SignUpComponent },
+  {path: 'sign-in', loadChildren: () => import('../sign-in/sign-in.module').then(m => m.SignInModule)},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
