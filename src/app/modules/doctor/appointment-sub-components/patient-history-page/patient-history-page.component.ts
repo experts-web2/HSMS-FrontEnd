cc5abf8db2c56c5f094c9d6564a87967944ee7d1
Experts Web 2 <experts.web2@gmail.com>
2023-08-16 21:31:47 +0500

    Patient History Page

--- src/app/modules/doctor/appointment-sub-components/patient-history-page/patient-history-page.component.ts
+++ src/app/modules/doctor/appointment-sub-components/patient-history-page/patient-history-page.component.ts
@@ -5,13 +5,16 @@ import { IPrescription } from 'src/app/models/interfaces/Prescription';
 import { IToken } from 'src/app/models/interfaces/Token';
 import { ITableColumns } from 'src/app/models/interfaces/table-Columns';
 import { IVital } from 'src/app/models/vitals';
-import { MedicationService, VitalService } from 'src/app/services';
-import { PrescriptionService } from '../../../../services/prescription/prescription.service';
+import { MedicationService, VitalService, PrescriptionService, LoaderService } from 'src/app/services';
 import { LabOrderService } from '../../../../services/lab-order/lab-order.service';
 import { IPatient } from 'src/app/models/interfaces/patient-model';
 import { DoctorService } from '../../../../services/doctor/doctor.service';
 import { IDoctor } from 'src/app/models/interfaces/Doctor';
-import { patientData } from '../../../../../data';
+import { MedicationDosageEnum } from 'src/app/constants/Constants/MedicationDosage';
+import { MedicationDurationEnum } from 'src/app/constants/Constants/MedicationDuration';
+import { MedicationFrequencyEnum } from 'src/app/constants/Constants/MedicationFrequency';
+import { MedicationRouteEnum } from 'src/app/constants/Constants/MedicationRoute';
+import { MedicationInstructionEnum } from 'src/app/constants/Constants/MedicationInstructions';
 
 @Component({
   selector: 'app-patient-history-page',
@@ -19,7 +22,10 @@ import { patientData } from '../../../../../data';
   styleUrls: ['./patient-history-page.component.scss']
 })
 export class PatientHistoryPageComponent implements OnInit {
+
   historyData?: IDialogData;
+  medicationTableData: Array<IMedicationTableColumn> = [];
+  tokens: Array<IToken> = [];
   medicationTableColumns: Array<ITableColumns> = [
     {
       name: 'Sr#',
@@ -53,29 +59,40 @@ export class PatientHistoryPageComponent implements OnInit {
 
   constructor(
     private readonly dialogRef: DynamicDialogRef, 
-    private readonly dialogConfig: DynamicDialogConfig<{token: IToken, patient: IPatient}>,
+    private readonly dialogConfig: DynamicDialogConfig<{historyVisits: Array<IToken>, patient: IPatient}>,
     private readonly medicationService: MedicationService,
     private readonly vitalsService: VitalService,
     private readonly prescriptionService: PrescriptionService,
     private readonly labOrderService: LabOrderService,
-    private readonly doctorService: DoctorService
+    private readonly doctorService: DoctorService,
+    private readonly loaderService: LoaderService
   ){
-
+    
   }
 
   ngOnInit(): void {
     let tokenId = '';
     let doctorId = '';
     console.log(this.dialogConfig.data);
+    if(this.dialogConfig.data?.historyVisits.length){
+      this.tokens = this.dialogConfig.data?.historyVisits;
+    }
+
     this.historyData = {
-      token: this.dialogConfig.data?.token
+      token: this.dialogConfig.data?.historyVisits[0]
     }
 
     if(this.historyData.token) { 
       tokenId = this.historyData.token?.id;
-      doctorId = this.historyData.token.doctorName
+      doctorId = this.historyData.token.doctorId
     }
 
+    this.getAllData(tokenId, doctorId);
+  }
+
+  getAllData(tokenId: string, doctorId: string){
+    console.log({tokenId, doctorId});
+    
     this.getLabOrder(tokenId);
     this.getMedication(tokenId);
     this.getPrescription(tokenId);
@@ -84,21 +101,28 @@ export class PatientHistoryPageComponent implements OnInit {
   }
 
   medicationMapper(){
-    if (this.historyData?.mediction?.medicationDetails) {
-      this.historyData.mediction.medicationDetails.map((x, i) => {
-        // let medication: IMedicationTableColumn ={
-        //   number: i + 1,
-        //   name: x.med
-        // } 
-        return 
+    if (this.historyData?.medication?.medicationDetails) {
+      this.medicationTableData = this.historyData.medication.medicationDetails.map((x, i) => {
+        let medication: IMedicationTableColumn = {
+          number: i + 1,
+          name: x.medicineName,
+          duration: `${x.durationValue} ${MedicationDurationEnum[x.duration]}`,
+          dosage: `${x.dosageValue} ${MedicationDosageEnum[x.dosage]}`,
+          frequency: MedicationFrequencyEnum[x.frequency],
+          route: MedicationRouteEnum[x.route],
+          instruction: MedicationInstructionEnum[x.insturction]
+        } 
+        return medication;
       })
     }
   }
 
   getMedication(tokenId: string){
+
     this.medicationService.getMedicationByTokenId(tokenId).subscribe({
       next: (x) => {
-        if (this.historyData) this.historyData.mediction = x;
+        if (this.historyData) this.historyData.medication = x;
+        this.medicationMapper();
       },
       error: (err: Error) => {
 
@@ -107,9 +131,11 @@ export class PatientHistoryPageComponent implements OnInit {
   }
 
   getVitals(tokenId: string){
+
     this.vitalsService.getVitalsByTokenId(tokenId).subscribe({
       next: (x) => {
         if (this.historyData) this.historyData.vitals = x;
+
       },
       error: (err: Error) => {
 
@@ -118,9 +144,11 @@ export class PatientHistoryPageComponent implements OnInit {
   }
 
   getPrescription(tokenId: string){
+
     this.prescriptionService.getPrescriptionByTokenId(tokenId).subscribe({
       next: (x) => {
         if (this.historyData) this.historyData.prescription = x;
+
       },
       error: (err: Error) => {
 
@@ -129,9 +157,11 @@ export class PatientHistoryPageComponent implements OnInit {
   }
 
   getDoctor(doctorId: string){
+
     this.doctorService.getDoctorById(doctorId).subscribe({
       next: (x) => {
         if (this.historyData) this.historyData.doctor = x;
+
       },
       error: (err: Error) => {
 
@@ -153,7 +183,7 @@ export class PatientHistoryPageComponent implements OnInit {
 
 interface IDialogData{
   token?: IToken,
-  mediction?: IMedication,
+  medication?: IMedication,
   prescription?: IPrescription,
   vitals?: IVital,
   labOrder?: any,
---
+++ src/app/modules/doctor/patient-schedule/patient-schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-schedule',
+  templateUrl: './patient-schedule.component.html',
+  styleUrls: ['./patient-schedule.component.scss']
+})
+export class PatientScheduleComponent {
+
+}
